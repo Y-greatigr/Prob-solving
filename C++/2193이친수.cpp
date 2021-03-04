257d9c729563d8099be65110713f25d891ce70df
Y-greatigr <yhb1427@gmail.com>
2021-03-04 23:06:43 +0900

    21.03.04

---
+++ C++/2193이친수.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+long long dp[91];
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int n; cin >> n;
+	dp[1] = 1;
+	dp[2] = 1;
+	for (int i = 3; i <= n; i++)
+		dp[i] = dp[i - 1] + dp[i - 2];
+	cout << dp[n];
+	return 0;
+}
