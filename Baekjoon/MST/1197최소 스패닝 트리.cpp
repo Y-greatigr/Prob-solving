0582b7f8e970640e9da416370297bffac9df6d24
Y-greatigr <yhb1427@gmail.com>
2022-07-30 14:53:30 +0900

    22.07.30

---
+++ Baekjoon/MST/1197최소 스패닝 트리.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define FastIO cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+using namespace std;
+
+int parent[10'001];
+vector<tuple<int,int,int>> v;
+
+int Find(int cur){
+    if(parent[cur]==cur)return cur;
+    return parent[cur]= Find(parent[cur]);
+}
+void Union(int a, int b){
+    a= Find(a);
+    b= Find(b);
+    if(a==b)return;
+    if(a<b)parent[b]=a;
+    else parent[a]=b;
+}
+bool cmp(tuple<int,int,int> a, tuple<int,int,int> b){
+    return get<2>(a) < get<2>(b);
+}
+int main(){
+    FastIO;
+    int V,E, sum=0, cnt=0;cin>>V>>E;
+    for(int i=1;i<=V;i++)parent[i]=i;
+    for(int i=0;i<E;i++){
+        int a,b,c;cin>>a>>b>>c;
+        v.push_back({a,b,c});
+    }
+    sort(v.begin(), v.end(), cmp);
+    for(int i=0;i<E;i++){
+        auto [a, b, c] = v[i];
+        a=Find(a);
+        b=Find(b);
+        if(a==b)continue;
+        Union(a,b);
+        sum+=c;
+        cnt++;
+        if(cnt==V-1)break;
+    }
+    cout<<sum;
+    return 0;
+}
