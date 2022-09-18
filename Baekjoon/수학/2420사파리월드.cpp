26f414a97855a3faf8c3e67212a01bbcab2e9c3b
Y-greatigr <yhb1427@gmail.com>
2022-09-18 23:25:17 +0900

    22.09.18

---
+++ Baekjoon/수학/2420사파리월드.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define FastIO cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+using namespace std;
+int main(){
+    FastIO;
+    long long a,b;cin>>a>>b;
+    cout<<abs(a-b);
+    return 0;
+}
